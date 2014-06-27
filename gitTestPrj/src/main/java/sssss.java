d25942e6488a1e05bf07e47779550a8b3e0c439e
Icecream0211 <icecream0211@gmail.com>
2014-06-27 10:51:26 +0800

    Update sssss.java
    添加了另一个pull测试code

--- gitTestPrj/src/main/java/sssss.java
+++ gitTestPrj/src/main/java/sssss.java
@@ -3,5 +3,6 @@ package main.java;
 public class sssss {
 	public static void main(String[] args) {
 		System.out.println("this is my first git using");
+		System.out.println("this is test for pull");
 	}
 }
